73f052a62a8cfef082f1a524a2901f8669aa649e
kreanimator <backin.v@yandex.ru>
2024-05-07 22:55:42 +0300

    Added basic backend for fastapi

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# App object
+app = FastAPI()
+
+origins = ['https://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"foo": "baz"}
+
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+
+@app.get("/api/todo{id}")
+async def get_todo_by_id(id):
+    return 1
+
+
+@app.post("/api/todo")
+async def post_todo(todo):
+    return 1
+
+
+@app.put("/api/todo{id}")
+async def put_todo(id, data):
+    return 1
+
+
+@app.delete("/api/todo{id}")
+async def post_todo(id):
+    return 1
+
