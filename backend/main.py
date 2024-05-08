f4ca07f1de2bea9c6280902af902b08f163ae817
kreanimator <backin.v@yandex.ru>
2024-05-08 10:28:18 +0300

    Finished backend for mongodb table

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
+from backend.model import Todo
+from database import (
+    fetch_one_todo,
+    fetch_all_todos,
+    create_todo,
+    update_todo,
+    remove_todo,
+)
 
 # App object
 app = FastAPI()
@@ -22,25 +31,40 @@ def read_root():
 
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.get("/api/todo{title}", response_model=Todo)
+async def get_todo_by_id(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no TODO item with this title {title}")
 
 
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Bad request")
 
 
-@app.put("/api/todo{id}")
-async def put_todo(id, data):
-    return 1
+@app.put("/api/todo{title}", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no TODO item with this title {title}")
 
 
-@app.delete("/api/todo{id}")
-async def post_todo(id):
-    return 1
+@app.delete("/api/todo{title}")
+async def post_todo(title):
 
+    response = await remove_todo(title)
+    if response:
+        return "Had successfully deleted TODO item"
+    if response:
+        return response
+    raise HTTPException(404, f"There is no TODO item with this title {title}")
