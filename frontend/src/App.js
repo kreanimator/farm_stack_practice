a4f13b43296147fc44432590c78272ddbfad3c8b
kreanimator <backin.v@yandex.ru>
2024-05-08 13:51:46 +0300

    Added FE React

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+
+        <div className="App list-group-item justify-content-center align-items-center mx-auto"
+         style ={{"width": "400px", "backgroundColor": "white", "marginTop": "15px"}}>
+          <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">
+          Task Manager </h1>
+          </div>
+    </div>
+  );
+}
+
+export default App;
